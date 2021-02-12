7ca07adf1af1354b003913bba6d6e0d0a1ffe30e
Nolorer <64555811+Nolorer@users.noreply.github.com>
2021-02-12 13:40:46 -0300

    Updated to build 0212-21

--- Start Command Interface.bat
+++ Start Command Interface.bat
@@ -3,19 +3,22 @@ cls
 set %ERRORLEVEL%=0
 :menu1
 cls
+cd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 echo Welcome!
 echo.
 echo 1.Change Background
-echo 2.Shutdown Computer
-echo 3.Restart Computer
-echo 4.Add Startup Programs
+echo 2.Theme Settings
+echo 3.Shutdown Computer
+echo 4.Restart Computer
+echo 5.Add Startup Programs
 
-choice /c 1234 /m "Choose a option:"
+choice /c 12345 /m "Choose a option:"
 
 if %ERRORLEVEL%==1 (goto changebg)
-if %ERRORLEVEL%==2 (goto shut)
-if %ERRORLEVEL%==3 (goto shutres)
-if %ERRORLEVEL%==4 (goto startadd)
+if %ERRORLEVEL%==2 (goto themest)
+if %ERRORLEVEL%==3 (goto shut)
+if %ERRORLEVEL%==4 (goto shutres)
+if %ERRORLEVEL%==5 (goto startadd)
 
 
 :changebg
@@ -57,4 +60,46 @@ if exist "%startprompt%" (
 	pause
 	goto menu1
 )
+
+:themest
+cd "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Themes"
+echo 1.Change Theme
+echo 2.Toggle desktop icons changing by themes
+echo 3.Toggle mouse pointer changing by themes
+choice /c 123 /m "Choose a option:"
+
+if %ERRORLEVEL%==1 (goto themest_change)
+if %ERRORLEVEL%==2 (goto themest_deskicon)
+if %ERRORLEVEL%==3 (goto themest_pointertoggle)
+
+:themest_change
+set /p themeprompt="Type the name of the theme without the location or extension:"
+
+if exist "%themeprompt%".theme (
+
+	reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v CurrentTheme /t REG_SZ /d "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Themes\%themeprompt%.theme" /f
+	pause
+	choice /c YN /m "You need to restart to apply the theme, do you want to do it now?"
+	if %ERRORLEVEL%==1 (shutdown -r -f -t 0) else (goto menu1)
+	
+	) else (
+	
+	echo Error:The file you specified does not exist.
+	pause
+	goto menu1
+	
+	)
+
+:themest_deskicon
+choice /c YN /m "Are themes allowed to change desktop items when applied? (Y/N)"
+
+if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesDesktopIcons /t REG_DWORD /d 1 /f & goto menu1)
+if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesDesktopIcons /t REG_DWORD /d 0 /f & goto menu1)
+
+:themest_pointertoggle
+choice /c YN /m "Are themes allowed to change the mouse pointer when applied? (Y/N)"
+
+if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesMousePointers /t REG_DWORD /d 1 /f & goto menu1)
+if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesMousePointers /t REG_DWORD /d 0 /f & goto menu1)
+
 
