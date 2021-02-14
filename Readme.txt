6fae1fb400922e40e7404d98b7b9235f62400e6a
Nolorer <64555811+Nolorer@users.noreply.github.com>
2021-02-14 12:04:11 -0300

    Updated to 0214-21 and added the offline readme

---
+++ Readme.txt
@@ -0,0 +1,42 @@
+###############################################
+####Start Command Interface - Documentation####
+###############################################
+
+Summary:
+
+1.Description
+2.Requirements
+3.Options
+4.Version History
+
+
+1.Description:
+
+Start Command Interface(SCI) is a software which allows a common user to use and toggle different settings that would
+previously need changing Registry keys or going to hidden folders. It uses Batch to natively make changes to any Windows computer,
+it also uses a option choosing system so you don't need to know specific commands to acess it's features.
+
+2.Requirements:
+
+Start Command Interface can work on Microsoft Windows Vista or higher only.
+
+3.Options:
+
+SCI has options for different sections of the OS, this includes:
+
+-Themes
+-Startup
+-Shutdown and Restarting
+-Background
+
+4.Version History:
+
+This version history starts from the oldest version to the newest version.
+
+-1.0:
+
+Adds options for Background changing, shutdown or restarting the computer and add startup programs.
+
+-1.1:
+
+Adds theme changing with extra options.
--- Start Command Interface.bat
+++ Start Command Interface.bat
@@ -8,17 +8,19 @@ echo Welcome!
 echo.
 echo 1.Change Background
 echo 2.Theme Settings
-echo 3.Shutdown Computer
-echo 4.Restart Computer
-echo 5.Add Startup Programs
+echo 3.Startup Settings
+echo 4.Power Settings
 
-choice /c 12345 /m "Choose a option:"
 
+choice /c 1234 /m "Choose a option:"
+
+echo.
 if %ERRORLEVEL%==1 (goto changebg)
 if %ERRORLEVEL%==2 (goto themest)
-if %ERRORLEVEL%==3 (goto shut)
-if %ERRORLEVEL%==4 (goto shutres)
-if %ERRORLEVEL%==5 (goto startadd)
+if %ERRORLEVEL%==3 (goto startst)
+if %ERRORLEVEL%==4 (goto powerst)
+
+
 
 
 :changebg
@@ -38,29 +40,6 @@ if exist "%backprompt%".png (
 	
 	)
 	
-:shut
-choice /c YN /m "Do you want to force the shutdown(closes all programs without saving before shuting down):"
-if %ERRORLEVEL%==1 (shutdown -s -f -t 0) else (shutdown -s -t 0)
-
-:shutres
-choice /c YN /m "Do you want to force the restart(closes all programs without saving before restarting):"
-if %ERRORLEVEL%==1 (shutdown -r -f -t 0) else (shutdown -r -t 0)
-
-:startadd
-set /p startprompt="Type the location of the file with the extension:"
-if exist "%startprompt%" (
-	
-	copy "%startprompt%" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" > nul
-	pause
-	echo Done.
-	goto menu1
-	
-) else (
-	echo Error:The file you specified does not exist or it's in a different extension.
-	pause
-	goto menu1
-)
-
 :themest
 cd "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Themes"
 echo 1.Change Theme
@@ -91,15 +70,72 @@ if exist "%themeprompt%".theme (
 	)
 
 :themest_deskicon
-choice /c YN /m "Are themes allowed to change desktop items when applied? (Y/N)"
+choice /c YN /m "Are themes allowed to change desktop items when applied?"
 
 if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesDesktopIcons /t REG_DWORD /d 1 /f & goto menu1)
 if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesDesktopIcons /t REG_DWORD /d 0 /f & goto menu1)
 
 :themest_pointertoggle
-choice /c YN /m "Are themes allowed to change the mouse pointer when applied? (Y/N)"
+choice /c YN /m "Are themes allowed to change the mouse pointer when applied?"
 
 if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesMousePointers /t REG_DWORD /d 1 /f & goto menu1)
 if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v ThemeChangesMousePointers /t REG_DWORD /d 0 /f & goto menu1)
 
+:startst
+echo 1.Add Startup Programs
+echo 2.Toogle Start Menu at Logon
+echo 3.Set First Logon
+choice /c 123 /m "Choose a option:"
 
+if %ERRORLEVEL%==1 (goto startst_startadd)
+if %ERRORLEVEL%==2 (goto startst_logonstm)
+if %ERRORLEVEL%==3 (goto startst_firstlogon)
+
+:startst_logonstm
+choice /c YN /m "Will the start menu appear when you log in into Windows?"
+
+if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v OpenAtLogon /t REG_DWORD /d 1 /f & goto menu1)
+if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v OpenAtLogon /t REG_DWORD /d 0 /f & goto menu1)
+
+:startst_startadd
+set /p startprompt="Type the location of the file with the extension:"
+if exist "%startprompt%" (
+	
+	copy "%startprompt%" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" > nul
+	pause
+	echo Done.
+	goto menu1
+	
+) else (
+	echo Error:The file you specified does not exist or it's in a different extension.
+	pause
+	goto menu1
+)
+
+:startst_firstlogon
+choice /c YN /m "Do you want to redo your first logon(Make Windows reconfigure the user as it does in setup)?"
+
+if %ERRORLEVEL%==1 (reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v FirstLogon /t REG_DWORD /d 1 /f)
+if %ERRORLEVEL%==2 (reg add "HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v FirstLogon /t REG_DWORD /d 0 /f)
+set %ERRORLEVEL%=0
+
+choice /c YN /m "You need to restart to start the first boot, do you want to do it now?"
+if %ERRORLEVEL%==1 (shutdown -r -f -t 0) else (goto menu1)
+
+:powerst
+
+echo 1.Shutdown Computer
+echo 2.Restart Computer
+
+choice /c 12 /m "Choose a option:"
+
+if %ERRORLEVEL%==1 (goto powerst_shut)
+if %ERRORLEVEL%==2 (goto powerst_shutres)
+
+:powerst_shut
+choice /c YN /m "Do you want to force the shutdown(closes all programs without saving before shuting down):"
+if %ERRORLEVEL%==1 (shutdown -s -f -t 0) else (shutdown -s -t 0)
+
+:powerst_shutres
+choice /c YN /m "Do you want to force the restart(closes all programs without saving before restarting):"
+if %ERRORLEVEL%==1 (shutdown -r -f -t 0) else (shutdown -r -t 0)
